c866006c0da599e7c7b6c79c6e07e9603b100b78
zakariaqasbaji <71232196+zakariaqasbaji@users.noreply.github.com>
2021-04-17 18:23:17 +0200

    Delete src/vuesA directory

--- src/vuesA/includes/includes.php
+++
@@ -1,9 +0,0 @@
-<script src="../../vendor/jquery/jquery.min.js"></script>
-<script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!--  plugin JavaScript-->
-<script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
-
-<!-- scripts pour tous les pages-->
-<script src="../../js/sb-admin-2.min.js"></script>
-<script src="../../assets/scripts/scriptsA/allScripts.js"></script>
